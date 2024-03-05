28a40371dca5b5a33261c6e68c4beaac930fc695
IvanR96 <irios96@hotmail.com>
2024-03-05 13:44:10 -0800

    rules in ruby shown

---
+++ rules_test.rb
@@ -0,0 +1,27 @@
+#Ruby Rules
+
+# Naming a file
+snake_cased_file.rb
+
+
+# Assigning a variable
+forty_two = 42
+
+
+#defining a method
+def this_method
+    #do stuff
+end 
+
+#constant declaration
+
+FOUR = 'four'
+FIVE = 5
+
+# multi line do end blocks
+[1,2,3].each do |i|
+    #do stuff
+end
+
+#single line do end blocks
+[1,2,3].each { |i| do_stuff }
